50428a5997fee82faac3f22affdb36cdad9bdbad
Fernando Ibanez <fernandoibanez@Fernandos-iMac.home>
2017-12-04 22:28:28 +0000

    first commit

---
+++ spring-mvc-annotation/src/main/java/com/fibanez/spring4annotation/configuration/JpaConfiguration.java
@@ -0,0 +1,10 @@
+package com.fibanez.spring4annotation.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+//@Configuration
+//@EnableTransactionManagement
+//@PropertySource(value = { "classpath:application.properties" })
+public class JpaConfiguration {
+}
